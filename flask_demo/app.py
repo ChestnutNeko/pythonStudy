5dd9fa5a90d05f4bede57d893f6c95dac776778d
zhanshiyu <zhanshiyu@jd.com>
2021-06-29 09:49:59 +0800

    update

--- flask_demo/app.py
+++ flask_demo/app.py
@@ -2,9 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# 路由解析 通过用户访问的路径，匹配相应的函数
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! Liz'
+
+@app.route("/user/<name>")
+def hello(name):
+    return "hello, %s"%name
+
+@app.route("/user/<int:id>")
+def hello2(id):
+    return "你好，%d号的会员"%id
+# debug模式开启
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
